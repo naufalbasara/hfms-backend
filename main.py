7b74428052e9020a46f9514a3890c27d0944a8f2
naufalbasara <naufalbasara@outlook.com>
2024-03-02 17:20:23 +0700

    add: new joining method

---
+++ main.py
@@ -0,0 +1,32 @@
+import os, pandas as pd
+
+from tools.gsheet_conn import GSheet
+from preprocess.preprocess_data import Dataframe
+from preprocess.do_join import join
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    load_dotenv()
+    drive_path = '/Users/naufalbasara/naufalrb19@gmail.com - Google Drive/My Drive/Kuliah/Tugas Akhir/Final Project Shared Folder'
+    combined_sheet = GSheet('1U-M35MU4VLXQTtSWkM9xddJAAeh6Z7bjGZOwwPZGnS4')
+    variable_list_sheet = GSheet('1AZrKKjabT-tRUceCXFq8BA_aHQQTUNC0Mc5mKEo224I')
+    folder_path = os.path.join(drive_path, 'Dataset/Raw CSV/')
+
+    # df=pd.read_csv(os.path.join(drive_path, 'Dataset/Raw CSV/Dietary/Dietary Interview - Individual Foods, First Day.csv'))
+    # print(df.info())
+    # print("Columns: ")
+    # for i in range(len(df.columns)):
+    #     print(df.columns[i])
+    # print("Number of Cols: ", len(df.columns))
+    # print("DF Shape: ", df.shape)
+
+    # print()
+    # print()
+
+    # print(df[['SEQN', 'DR1ILINE']])
+    # print("SEQN Unique value counts: ", df['SEQN'].value_counts())
+    # print("DR1ILINE Unique value counts: ", df['DR1ILINE'].value_counts())
+
+
+    # Join all the data from NHANES
+    join(how='diet_aggregated', drive_path=drive_path, data_folder_path=folder_path, version=1)
--- preprocess/do_join.py
+++ preprocess/do_join.py
@@ -17,96 +17,117 @@ def variable_mapping(variable_file_sheet_key='1AZrKKjabT-tRUceCXFq8BA_aHQQTUNC0M
 
     return variable_to_description
 
-if __name__ == '__main__':
-    drive_path = '/Users/naufalbasara/naufalrb19@gmail.com - Google Drive/My Drive/Kuliah/Tugas Akhir/Final Project Shared Folder'
-    combined_sheet = GSheet('1U-M35MU4VLXQTtSWkM9xddJAAeh6Z7bjGZOwwPZGnS4')
+def join(how, drive_path, data_folder_path, version):
     variable_list_sheet = GSheet('1AZrKKjabT-tRUceCXFq8BA_aHQQTUNC0Mc5mKEo224I')
-    folder_path = os.path.join(drive_path, 'Dataset/Raw CSV/')
-
-    file_used = {}
-
-    # Get all the used variable files in a dictionary
-    # do something here..
-    variable_list = variable_list_sheet.to_df(variable_list_sheet.open_wks('Sheet1'))
-    variable_list = variable_list[variable_list['is_used'] == 'TRUE']
-
-    for _, row in variable_list.iterrows():
-        if row['name'] not in file_used:
-            print(f'Creating list of files for {row["name"]}')
-            file_used[row['name']] = []
-        
-        if row['title'] not in file_used[row['name']]:
-            file_used[row['name']].append(row['title'])
-
-    print("Done creating list for used files.")
-    
-    # Combine all the files by type
-    # e.g. Demographic -> Demographic_Combined
-    # e.g. Dietary1, Dietary2 -> Dietary_Combined
-    print()
-    combined_all = None
-    for name, files in file_used.items():
-        combined = None
-        combined_name = name
-        print(f'Merging all files in {name}...')
-        
-        for count, file in enumerate(files):
-            try:
-                file_code = f'{name[:5]}{count+1}'
-                file_path = os.path.join(folder_path, name, f'{file.strip()}.csv')
-                df = pd.read_csv(file_path)
-
-                used_variables = variable_list[(variable_list['name'] == name) & (variable_list['title'] == file)]['variable'].tolist()
-                df = df.loc[:, used_variables]
-
-                old_ = df.columns
-                new_ = [*df.columns]
-                new_.remove('SEQN')
-                new_ = [*map(lambda x: f'{file_code}_{x}', new_)]
-                new_.insert(0, 'SEQN')
-
-                column_mapping = dict(map(lambda x,y: (x,y), old_, new_))
-                
-                # rename all columns according to the survey type
-                df.rename(columns=column_mapping, inplace=True)
-
-                if type(combined) == type(None):
-                    combined = df
-                    continue
-
-                combined = pd.merge(combined, df, how='outer', on='SEQN')
-            except Exception as e:
-                print(f"[FAILED]: Error in file {file}, {e}")
-        
-        if type(combined_all) == type(None):
-            combined_all = combined
-            continue
-        
-        combined_all = pd.merge(combined_all, combined, how='outer', on='SEQN')
-
-        print(f'Merging files in {name} completed.')
-        print()
-
-        try:
-            combined_sheet.trunc_ins(name, combined)
-            print("Successfully updated to sheets \"Review Combined\"")
-        except:
-            print("[FAILED]: Unable to store in sheet..")
-            print("Proceed to store in drive under the folder Dataset/Merged_Backup...")
-            combined.to_csv(os.path.join(drive_path, 'Dataset/Merged_Backup', f'Combined_{name}.csv'))
-            print("File stored.")
-        
-        print(f'---\t\t{name} COMPLETED\t\t---')
+    result_combined_sheet = GSheet('1U-M35MU4VLXQTtSWkM9xddJAAeh6Z7bjGZOwwPZGnS4')
+    # log_sheet = GSheet('1rY5qjPzRNPcjzojSGvCyY-Gzj4-KD3oPAuVHviiVW2A')
     
     try:
-        combined_sheet.trunc_ins('ALL', combined_all)
-        print("Successfully updated to sheets \"ALL\"")
-    except:
-        print("[FAILED]: Unable to store Combined_All in sheet..")
-        print("Proceed to store in drive under the folder Dataset/Merged_Backup...")
-        combined_all.to_csv(os.path.join(drive_path, 'Dataset/Merged_Backup', f'Combined_All.csv'))
-        print("All combination file stored.")
-    
+        print(f'-----\t\tPROCESS STARTED\t\t-----')
+        file_used = {}
 
-    print(f'---\t\tALL COMPLETED\t\t---')
-    print('\n\n')
+        # Get all the used variable files in a dictionary
+        # do something here..
+        variable_list = variable_list_sheet.to_df(variable_list_sheet.open_wks('Sheet1'))
+        variable_list = variable_list[variable_list['is_used'] == 'TRUE']
+
+        for _, row in variable_list.iterrows():
+            if row['name'] not in file_used:
+                print(f'Creating list of files for {row["name"]}')
+                file_used[row['name']] = []
+                
+            if row['title'] not in file_used[row['name']]:
+                if how in ['diet_all', 'diet_aggregated', 'diet_separated']:
+                    # do normal join
+                    file_used[row['name']].append(row['title'])
+
+                elif how == 'diet_ignored':
+                    # skipped the data for respondent daily food consumption and only take the daily nutrition
+                    if row['title'] in ['Dietary Interview - Individual Foods, First Day', 'Dietary Interview - Individual Foods, Second Day']:
+                        continue
+
+        print("Done creating list for used files.\n")
+            
+        # Combine all the files by type
+        # e.g. Demographic -> Demographic_Combined
+        # e.g. Dietary1, Dietary2 -> Dietary_Combined
+        combined_all = None
+        for name, files in file_used.items():
+            print(f'---\t\t{name} STARTED\t\t---')
+            combined = None
+            print(f'Merging all files in {name}...')
+                
+            for count, file in enumerate(files):
+                try:
+                    file_code = f'{name[:5]}{count+1}'
+                    file_path = os.path.join(data_folder_path, name, f'{file.strip()}.csv')
+                    df = pd.read_csv(file_path)
+
+                    used_variables = variable_list[(variable_list['name'] == name) & (variable_list['title'] == file)]['variable'].tolist()
+                    df = df.loc[:, used_variables]
+
+                    # join on the user's input
+                    if file.strip() in ['Dietary Interview - Individual Foods, First Day', 'Dietary Interview - Individual Foods, Second Day']:
+                        if how=='diet_aggregated':
+                            # do join by aggregating all the user's record on one day food consumption
+                            df = df.groupby(['SEQN', 'DRDINT'], dropna=False, as_index=False).mean()
+                            
+                        elif how=='diet_separated':
+                            # do join by separating all the user's record on one day food consumption and eating occasion
+                            pass
+                    
+                    # change the columns to respective name
+                    old_ = df.columns
+                    new_ = [*df.columns]
+                    new_.remove('SEQN')
+                    new_ = [*map(lambda x: f'{file_code}_{x}', new_)]
+                    new_.insert(0, 'SEQN')
+
+                    column_mapping = dict(map(lambda x,y: (x,y), old_, new_))
+                        
+                    # rename all columns according to the survey type
+                    df.rename(columns=column_mapping, inplace=True)
+
+                    if type(combined) == type(None):
+                        combined = df
+                        continue
+
+                    combined = pd.merge(combined, df, how='outer', on='SEQN')
+                except Exception as e:
+                    print(f"[FAILED]: Error in file {file}, {e}")
+                
+            if type(combined_all) == type(None):
+                combined_all = combined
+            else:
+                combined_all = pd.merge(combined_all, combined, how='outer', on='SEQN')
+
+            print(f'Merging files in {name} completed.')
+            print()
+
+            try:
+                result_combined_sheet.trunc_ins(name, combined)
+                print("Successfully updated to sheets \"Review Combined\"")
+                print(f"Storing Combined_{name}...")
+                combined.to_csv(os.path.join(drive_path, 'Dataset/Data Versioning/Data', f'Combined_{name}.csv'))
+                print("File stored in drive under the folder Dataset/Data Versioning.")
+            except:
+                print(f"[FAILED]: Unable to store Combined_{name}.csv in sheet..")
+            finally:
+                print(f'---\t\t{name} COMPLETED\t\t---')
+            
+        try:
+            result_combined_sheet.trunc_ins('ALL', combined_all)
+            print("Successfully updated to sheets \"ALL\"")
+            print("Storing all combined file in drive...")
+            combined_all.to_csv(os.path.join(drive_path, 'Dataset/Data Versioning', f'Combined_All_V{version}.csv'))
+            print("All combination file stored in drive under the folder Dataset/Data Versioning.")
+        except:
+            print(f"[FAILED]: Unable to store Combined_All_V{version} in sheet..")
+
+        print(f'-----\t\t$PROCESS COMPLETED$\t\t-----')
+        print('\n\n')
+    except Exception as e:
+        print(e)
+        return False
+    finally:
+        return True
