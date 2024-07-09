62d56df4731bcf78de8ba038ac77cf53fcace0ca
naufalbasara <naufalbasara@outlook.com>
2024-07-09 23:52:29 +0700

    chore: deployment configs

--- flaskr/webserver.py
+++ flaskr/webserver.py
@@ -454,4 +454,4 @@ def app_recommendation(version):
 # ===================== End of Application Endpoints =====================
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
